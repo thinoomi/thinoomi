f125f37c7e37bba44d854bcc302cee2c333c8b41
thinoomi <86285014+thinoomi@users.noreply.github.com>
2022-03-24 01:20:08 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,92 @@
-
-$(document).ready(function(){
+//기존에 격자들 상태 off로 해놓은거임
+$(document).ready(function () {
   $('.box').val("off");
-
 });
 
+//바깥 클릭했을때 용암 연출한거임
+function hotani() {
 
-var playAlert = setInterval(function(){
-  if ( $('.item001').val() == "on" && $('.item002').val() == "on"){
-    $('.answer').css({"opacity": "1"});
+  $('.Q').animate({ opacity: '0.5' })
+    .delay(1500)
+    .animate({ opacity: '1' });
+
+  $('.red').animate({ opacity: '0.3' })
+    .delay(1500)
+    .animate({ opacity: '0' });
+
+  $('.goback').animate({ opacity: '1' })
+    .delay(1500)
+    .animate({ opacity: '0' });
+}
+
+//정답 처리 하려고 변수 지정한거임 처음엔 0
+var totalvalue = 0;
+//토탈밸류가 일정 수일때 정답 처리가 되게 할거임
+var playAlert = setInterval(function () {
+  if (totalvalue == 23) {
+    $('.answer').animate({ opacity: '1' });
   }
-  else {$('.answer').css({"opacity": "0"});}
+  else {
+    $('.answer').animate({ opacity: '0' })
+  }
 }, 500);
 
-$(function(){
-  $('.box').click(function(){
+//정답 연산부분. 클릭시 진행되는함수인거임.
+$(function () {
+  $('.box').click(function () {
     isPaused = false;
-    if ($(this).val() == "off"){
-    $(this).css({"opacity": "1"});
-    $(this).val("on");
-    $('.box').mouseleave(function(){
-      $(this).css({"opacity": "1"});
-    });
-    }
-    else{
-      $(this).css({"opacity": "0"});
-      $(this).val("off");
-      $('.box').mouseleave(function(){
-        $(this).css({"opacity": "0"});
+    if ($(this).val() == "off") {
+      $(this).css({ "opacity": "1" });
+      $(this).val("on");
+      $('.box').mouseleave(function () {
+        $(this).css({ "opacity": "1" });
       });
+
+      if ($(this).attr('type') == "b") {
+        totalvalue += 1;
+      }
+      else {
+        totalvalue -= 1000;
+      }
+
+    }
+
+
+    else {
+      $(this).css({ "opacity": "0" });
+      $(this).val("off");
+      $('.box').mouseleave(function () {
+        $(this).css({ "opacity": "0" });
+      });
+
+      if ($(this).attr('type') == "b") {
+        totalvalue -= 1;
+      }
+      else {
+        totalvalue += 1000;
+      }
+
     }
   });
   return 0;
 });
 
+//여기는 마우스 올렸을때 실루엣 보이는 효과임 끗 아 쉽다~
+$(function () {
+  $('.box').mouseover(function () {
+    $(this).css({ "opacity": "0.3" });
 
-$(function(){
-  $('.box').mouseover(function(){
-    $(this).css({"opacity": "0.3"});
-
-    if ($(this).val() == "off"){
-      $('.box').mouseleave(function(){
-        $(this).css({"opacity": "0"});
+    if ($(this).val() == "off") {
+      $('.box').mouseleave(function () {
+        $(this).css({ "opacity": "0" });
       });
-  }
+    }
 
-  else{
-    $('.box').mouseleave(function(){
-      $(this).css({"opacity": "1"});
-  });
-}
+    else {
+      $('.box').mouseleave(function () {
+        $(this).css({ "opacity": "1" });
+      });
+    }
 
   });
 });
